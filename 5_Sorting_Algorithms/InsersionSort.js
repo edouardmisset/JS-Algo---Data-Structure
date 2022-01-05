0e396409e50c247eb75f5ab3c62093b7bdb79d3a
Edouard Misset <edouardmisset@gmail.com>
2022-01-05 12:18:31 +0100

    File renaming

---
+++ 5_Sorting_Algorithms/InsersionSort.js
@@ -0,0 +1,11 @@
+
+function insertionSort(arr) {
+  // Swapping logique
+  // Just numerical comparaison here
+  const swap = (arr, index1, index2) => [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
+
+
+  return arr
+}
+
+console.log(insertionSort([7, 3, 5, 1]))
