392cf9607d7769c6df4b36f6abb53b044e19b704
Edouard Misset <edouardmisset@gmail.com>
2022-02-03 11:35:06 +0100

    Weighted Graph Structure in preparation for the Dijkstra's Algo

--- 13_Graphs/Graph.ts
+++ 13_Graphs/Graph.ts
@@ -6,15 +6,16 @@
 // }
 
 // Undirected graph
+// Unweighted graph
 class Graph {
   public adjacencyList: any;
   constructor() {
     this.adjacencyList = {};
   }
 
-  addVertex(name: string): void {
+  addVertex(vertex: string): void {
     // TODO: Should check if the vertex already exists and return a warning | Error handling
-    if (!this.adjacencyList[name]) this.adjacencyList[name] = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
   addEdge(vertex1: string, vertex2: string): void {
@@ -85,7 +86,6 @@ class Graph {
       if (currentVertex) {
         result.push(currentVertex)
         visited[currentVertex] = true
-
         this.adjacencyList[currentVertex].forEach((neighbor: string): void => {
           if (!visited[neighbor]) {
             queue.push(neighbor)
@@ -99,24 +99,57 @@ class Graph {
   }
 }
 
-const graph = new Graph();
+// const graph = new Graph();
 
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
+// graph.addVertex('A');
+// graph.addVertex('B');
+// graph.addVertex('C');
+// graph.addVertex('D');
+// graph.addVertex('E');
+// graph.addVertex('F');
 
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'E');
-graph.addEdge('D', 'F');
-graph.addEdge('E', 'F');
-console.log(graph.adjacencyList);
+// graph.addEdge('A', 'B');
+// graph.addEdge('A', 'C');
+// graph.addEdge('B', 'D');
+// graph.addEdge('C', 'E');
+// graph.addEdge('D', 'E');
+// graph.addEdge('D', 'F');
+// graph.addEdge('E', 'F');
+// console.log(graph.adjacencyList);
 
-console.log(graph.depthFirstSearchRecursive('A'));
-console.log(graph.depthFirstSearchIterative('A'));
-console.log(graph.breadthFirstSearch('A'));
+// console.log(graph.depthFirstSearchRecursive('A'));
+// console.log(graph.depthFirstSearchIterative('A'));
+// console.log(graph.breadthFirstSearch('A'));
+
+
+class WeightedGraph {
+  public adjacencyList: any;
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+  }
+  addEdge(vertex1: string, vertex2: string, weight: number) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
+
+const weightedGraph = new WeightedGraph()
+weightedGraph.addVertex("A")
+weightedGraph.addVertex("B")
+weightedGraph.addVertex("C")
+weightedGraph.addVertex("D")
+weightedGraph.addVertex("E")
+weightedGraph.addVertex("F")
+weightedGraph.addEdge("A", "B", 4)
+weightedGraph.addEdge("A", "C", 2)
+weightedGraph.addEdge("B", "E", 3)
+weightedGraph.addEdge("C", "D", 2)
+weightedGraph.addEdge("C", "F", 4)
+weightedGraph.addEdge("D", "F", 1)
+weightedGraph.addEdge("D", "E", 3)
+weightedGraph.addEdge("F", "E", 1)
+
+console.log(weightedGraph.adjacencyList)
